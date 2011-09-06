76fcb8dab16249de361ae37904b44df1ba4a6522
Anthony Mills <amills@emailmail.net>
2011-09-06 11:57:44 +1000

    basic SQL schema to support playliast

---
+++ includes/s3_video.sql
@@ -0,0 +1,14 @@
+CREATE TABLE IF NOT EXISTS `s3_video_playlists` (
+  `id` int(8) NOT NULL AUTO_INCREMENT,
+  `playlist_name` varchar(100) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+
+CREATE TABLE IF NOT EXISTS `s3_video_playlist_videos` (
+  `id` int(10) NOT NULL AUTO_INCREMENT,
+  `video_file` varchar(200) NOT NULL,
+  `video_playlist` int(11) NOT NULL,
+  `video_weight` int(5) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+
