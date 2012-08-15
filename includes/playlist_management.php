bdcdd418eabef58fc753080cc0efa61d9b2a86e5
Anthony Mills <amills@emailmail.net>
2012-08-15 15:23:22 +1000

    Added doc blocks to playlist_management class

--- includes/playlist_management.php
+++ includes/playlist_management.php
@@ -3,6 +3,11 @@ class s3_playlist_management
 {
 	protected $_playlistName = NULL;
 	
+	/**
+	 * Set the database id of the playlist
+	 * 
+	 * @param integer $playList
+	 */
 	public function setPlaylist($playList = NULL) 
 	{
 		if (!empty($playList)) {
@@ -10,6 +15,15 @@ class s3_playlist_management
 		}
 	}
 	
+	/**
+	 * 
+	 * Save playlist to the database
+	 * 
+	 * @param string $playlistName
+	 * @param array $playlistContents
+	 * 
+	 * @return integer
+	 */
 	public function createPlaylist($playlistName, $playlistContents)
 	{
 		$playlistId = $this->_savePlaylistName($playlistName);
@@ -25,6 +39,15 @@ class s3_playlist_management
 		return $playlistId;		
 	}
 	
+	/**
+	 * 
+	 * Update the contents of a video stored in the database
+	 * 
+	 * @param integer $playlistId
+	 * @param array $playlistContents
+	 * 
+	 * @return boolean
+	 */
 	public function updatePlaylistVideos($playlistId, $playlistContents)
 	{
 		if (!$playlistId) {
@@ -36,20 +59,38 @@ class s3_playlist_management
 			$videoId = $this->_addVideoToPlaylist($fileName, $playlistId, $x); 
 			$x++;	
 		}
-		return $playlistId;				
+		return TRUE;				
 	}
 	
+	/**
+	 * 
+	 * Delete a playlist stored in the database
+	 * 
+	 * @param $playlistId
+	 */
 	public function deletePlaylist($playlistId)
 	{
 		mysql_query("DELETE FROM s3_video_playlists WHERE id = '$playlistId'");
 		$this->deletePlaylistVideos($playlistId);
 	}
 	
+	/**
+	 * 
+	 * Delete all of the videos belonging to a playlist from the database
+	 * 
+	 * @param integer $playlistId
+	 */
 	public function deletePlaylistVideos($playlistId)
 	{
 		mysql_query("DELETE FROM s3_video_playlist_videos WHERE video_playlist = '$playlistId'")  or die(mysql_error());			
 	}
 	
+	/**
+	 * 
+	 * Fetch all of the playlists currently stored in the database
+	 * 
+	 * @return array
+	 */
 	public function getAllPlaylists() 
 	{
 		$playlists = mysql_query("SELECT * FROM s3_video_playlists");
@@ -62,6 +103,13 @@ class s3_playlist_management
 		return $existingPlaylists;	
 	}
 
+	/**
+	 * 
+	 * Fetch a playlist from the database via its name
+	 * 
+	 * @param string $playlistName
+	 * @return array
+	 */
 	public function getPlaylistsByTitle($playlistName) 
 	{
 		$playlists = mysql_query("SELECT * FROM s3_video_playlists WHERE playlist_name LIKE '$playlistName'");
@@ -73,6 +121,14 @@ class s3_playlist_management
 		return $existingPlaylists;	
 	}
 		
+	/**
+	 * 
+	 * Fetch the videos that belong to a playlist from the database
+	 * 
+	 * @param integer $playlistId
+	 * 
+	 * @return array
+	 */	
 	public function getPlaylistVideos($playlistId)
 	{
 		$videos = mysql_query("SELECT * FROM s3_video_playlist_videos WHERE video_playlist LIKE '$playlistId' ORDER BY video_weight ASC");
@@ -84,6 +140,12 @@ class s3_playlist_management
 		return $playlistVideos;			
 	}
 		
+	/**
+	 * 
+	 * Save a new playlist to the database
+	 * 
+	 * @param string $playlistName
+	 */	
 	protected function _savePlaylistName($playlistName) 
 	{
 		$time = time();
@@ -91,6 +153,16 @@ class s3_playlist_management
 		return mysql_insert_id();
 	}
 	
+	/**
+	 * 
+	 * Add a video to the playlist
+	 * 
+	 * @param string $fileName
+	 * @param integer $playlistId
+	 * @param integer $weight
+	 * 
+	 * @return integer
+	 */
 	protected function _addVideoToPlaylist($fileName, $playlistId, $weight = 1) {
 		mysql_query("INSERT INTO s3_video_playlist_videos (video_file, video_playlist, video_weight) VALUES ('$fileName', '$playlistId', '$weight')");		
 		return mysql_insert_id();		
